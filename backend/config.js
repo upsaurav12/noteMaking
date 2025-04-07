f655913880703152b23cf8136c728f924bb0ec59
upsaurav12 <sauravup041103@gmail.com>
2025-04-07 09:15:40 +0530

    added mongodb and express to the project

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5000
+
+export const MONGO_DB_URL = 'mongodb://localhost:27017'
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 import express from 'express'
-
+import mongoose, { connect } from 'mongoose'
+import { PORT  , MONGO_DB_URL} from './config.js'
 const app = express()
+
+mongoose
+.connect(MONGO_DB_URL)
+.then(() => {
+    console.log("MongoDB is connected successfully")
+    app.listen(PORT , () => {
+        console.log(`The Apps is running on the PORT ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
